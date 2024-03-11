368968c6ce715f03551584efd1e60e5aa0b0325e
rebi13 <rebi13@naver.com>
2024-03-11 23:32:16 +0900

    style: 스타일 컴포넌트 분리 rebi13/MBTI-Inside#73

--- frontend/src/components/common/Alert/index.tsx
+++ frontend/src/components/common/Alert/index.tsx
@@ -1,18 +1,15 @@
-import { BsExclamationCircleFill } from 'react-icons/bs';
-
+import * as S from '@/components/common/Alert/styles';
 import Button from '@/components/common/Button';
 
 const Alert = () => {
   return (
-    <div role="alert" className="flex flex-col gap-2 items-center">
-      <BsExclamationCircleFill className="text-4xl text-yellow-400" />
-      <div className="flex h-10 items-center text-lg">
-        제목을 입력해 주세요.
-      </div>
+    <S.AlertContainer role="alert">
+      <S.AlertIcon />
+      <S.AlertContent>제목을 입력해 주세요.</S.AlertContent>
       <div>
         <Button classProp={'h-8 rounded-md'}>확인</Button>
       </div>
-    </div>
+    </S.AlertContainer>
   );
 };
 
