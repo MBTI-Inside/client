4c88e3820eb1ec666d985f912db4e6935ab8b362
rebi13 <rebi13@naver.com>
2024-03-12 00:12:47 +0900

    style: 관리자 메인 페이지 rebi13/MBTI-Inside#75

--- frontend/src/pages/Admin/index.tsx
+++ frontend/src/pages/Admin/index.tsx
@@ -1,5 +1,26 @@
+import Button from '@/components/common/Button';
+import Character from '@/components/common/Character';
+import SmallCharacter from '@/components/common/SmallCharacter';
+
 const Admin = () => {
-  return <>Admin</>;
+  return (
+    <div className="flex flex-col my-5">
+      <Character bgcolor="#D5BA38" gcolor="#7FBB88" />
+      <h3
+        className="font-bold
+            text-6xl
+            text-center
+            pb-[60px]
+            text-[#000]
+            bg-[#D5BA38]"
+      >
+        Admin 😎
+      </h3>
+      <Button classProp={'text-xl'}>테스트 관리</Button>
+      <Button classProp={'text-xl'}>게시글 관리</Button>
+      <Button classProp={'text-xl'}>사용자 관리</Button>
+    </div>
+  );
 };
 
 export default Admin;
