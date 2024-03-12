78413dd21fb650feb0683d1a86d365aa487fb8cf
rebi13 <rebi13@naver.com>
2024-03-12 23:59:13 +0900

    style: 관리자 문항 뷰 초안 rebi13/MBTI-Inside#75

--- frontend/src/pages/Admin/Question/View/index.tsx
+++ frontend/src/pages/Admin/Question/View/index.tsx
@@ -1,5 +1,53 @@
+import { CiMenuKebab } from 'react-icons/ci';
+import { SlArrowLeft } from 'react-icons/sl';
+
+import Button from '@/components/common/Button';
+
 const QuestionView = () => {
-  return <>QuestionView</>;
+  return (
+    <main className="flex flex-1 flex-col gap-4">
+      <div className="flex flex-row justify-between items-center">
+        <Button classProp="btn-outline border-none">
+          <SlArrowLeft />
+        </Button>
+        <span>Question</span>
+        <Button classProp="btn-outline border-none">
+          <CiMenuKebab />
+        </Button>
+      </div>
+      <section
+        className="w-[330px] rounded-3xl h-[80%]
+    bg-yellow-300 p-4 opacity-100 flex-1
+    flex flex-col"
+      >
+        <section>
+          <div>질문</div>
+          <div>
+            바쁜 회사 생활을 보낸 당신. 황금 같은 주말을 어떻게 보내려고 할까?
+          </div>
+        </section>
+        <section>
+          <div>답변</div>
+          <div>집에서 놀아야지!</div>
+          <div>밖에서 놀아야지!</div>
+        </section>
+
+        <section>
+          <div>문항 유형</div>
+          <div>E(energy)</div>
+        </section>
+        <section>
+          <div>비중도</div>
+          <progress
+            className="progress progress-primary w-56 bg-yellow-200"
+            value="73"
+            max="100"
+          ></progress>
+          <div>73%</div>
+        </section>
+      </section>
+    </main>
+  );
 };
 
 export default QuestionView;
