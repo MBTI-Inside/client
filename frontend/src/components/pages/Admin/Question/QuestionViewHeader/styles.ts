8cdff4b0d5e54f124652f2e6281d8143353dbf3c
rebi13 <rebi13@naver.com>
2024-03-13 23:59:07 +0900

    style: 컴포넌트 분리 및 스타일링 rebi13/MBTI-Inside#75

---
+++ frontend/src/components/pages/Admin/Question/QuestionViewHeader/styles.ts
@@ -0,0 +1,25 @@
+import tw from 'tailwind-styled-components';
+
+export const DropDowns = tw.div`
+    dropdown 
+    dropdown-end
+`;
+
+export const DropDownButton = tw.div`
+    btn 
+    m-1 
+    bg-inherit 
+    border-none 
+    text-xl
+`;
+
+export const DropDownContents = tw.ul`
+    dropdown-content 
+    z-[1] 
+    menu 
+    p-2 
+    shadow 
+    bg-base-100 
+    rounded-box 
+    w-24
+`;
