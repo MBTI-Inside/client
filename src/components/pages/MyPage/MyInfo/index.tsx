a36fb8d1fc1b64a705dbb3cb77c2c1e6d47e31fe
rebi13 <rebi13@naver.com>
2024-03-26 00:20:51 +0900

    style: MyInfo 컴포넌트 스타일링 MBTI-Inside#7

--- src/components/pages/MyPage/MyInfo/index.tsx
+++ src/components/pages/MyPage/MyInfo/index.tsx
@@ -1,26 +1,26 @@
 import { FaPencilAlt } from 'react-icons/fa';
 
-const MyInfo = () => {
-  // https://avatars.githubusercontent.com/u/63568239?v=4
+import * as S from '@/components/pages/MyPage/MyInfo/styles';
 
+const MyInfo = () => {
   return (
-    <div className="flex flex-row bg-white items-center justify-around w-full h-32 gap-2 rounded-lg">
+    <S.MyInfoContainer>
       <div className="avatar">
         <div className="w-20 rounded">
           <img src="https://avatars.githubusercontent.com/u/63568239?v=4" />
         </div>
       </div>
-      <div className="flex flex-col gap-4">
-        <div className="flex justify-end items-center gap-2">
+      <S.MyInfoAreaWrapper>
+        <S.MyInfoArea>
           <div className="flex gap-1">
             <span className="font-bold">언정차</span>
             <span>님</span>
           </div>
           <FaPencilAlt className="text-lg" />
-        </div>
+        </S.MyInfoArea>
         <div>MBTI : ESTJ</div>
-      </div>
-    </div>
+      </S.MyInfoAreaWrapper>
+    </S.MyInfoContainer>
   );
 };
 
