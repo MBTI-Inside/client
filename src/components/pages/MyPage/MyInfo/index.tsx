a15a957fd0b2deea8c196839ca673f2017288ba9
rebi13 <rebi13@naver.com>
2024-03-25 23:14:44 +0900

    style: 마이페이지 프로필 카드 영역 퍼블리싱 MBTI-Inside#7

--- src/components/pages/MyPage/MyInfo/index.tsx
+++ src/components/pages/MyPage/MyInfo/index.tsx
@@ -1,17 +1,24 @@
 import { FaPencilAlt } from 'react-icons/fa';
 
 const MyInfo = () => {
+  // https://avatars.githubusercontent.com/u/63568239?v=4
+
   return (
-    <div className="flex flex-row bg-white items-center justify-center w-full">
-      <div>photo</div>
-      <div className="flex flex-col">
-        <div>
-          <span>
-            언정차님
-            <FaPencilAlt />
-          </span>
+    <div className="flex flex-row bg-white items-center justify-around w-full h-32 gap-2 rounded-lg">
+      <div className="avatar">
+        <div className="w-20 rounded">
+          <img src="https://avatars.githubusercontent.com/u/63568239?v=4" />
         </div>
-        <div>MBTI: ESTJ</div>
+      </div>
+      <div className="flex flex-col gap-4">
+        <div className="flex justify-end items-center gap-2">
+          <div className="flex gap-1">
+            <span className="font-bold">언정차</span>
+            <span>님</span>
+          </div>
+          <FaPencilAlt className="text-lg" />
+        </div>
+        <div>MBTI : ESTJ</div>
       </div>
     </div>
   );
