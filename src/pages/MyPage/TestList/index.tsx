3b527cbc88214bd5351c5cad20340064542f56b6
rebi13 <rebi13@naver.com>
2024-03-27 00:03:03 +0900

    style: 마이페이지 메인 퍼블리싱 MBTI-Inside#8

--- src/pages/MyPage/TestList/index.tsx
+++ src/pages/MyPage/TestList/index.tsx
@@ -1,4 +1,5 @@
 import { CiMenuKebab } from 'react-icons/ci';
+import { GrAdobeCreativeCloud } from 'react-icons/gr';
 import { IoIosArrowBack } from 'react-icons/io';
 
 import Character from '@/components/common/Character';
@@ -7,30 +8,90 @@ import * as S from '@/components/pages/Admin/Question/QuestionViewHeader/styles'
 
 const MyTestList = () => {
   return (
-    <>
-      <S.QuestionViewHeaderContainer>
-        <div className="pl-4 text-xl">
+    <main className="flex flex-1 flex-col gap-3 items-center">
+      <div className="flex items-center w-full justify-between h-12">
+        <div className="text-xl text-white">
           <IoIosArrowBack onClick={() => {}} />
         </div>
-        <span className="font-bold text-xl">내 MBTI</span>
-        <div>ㄱ</div>
-      </S.QuestionViewHeaderContainer>
-
-      <div className="card w-5/6 bg-base-100 shadow-xl">
+        <span className="font-bold text-xl text-white">내 MBTI</span>
+        <div className="text-xl text-white">
+          <GrAdobeCreativeCloud />
+        </div>
+      </div>
+      <section className="card w-80 bg-base-100 shadow-xl">
         <figure className="px-10 pt-10">
           <Character bgcolor="#BF1132" gcolor="#FF98CA" />
         </figure>
-        <div className="card-body items-center text-center">
-          <progress className="progress" value="10" max="100" />
-          <progress className="progress" value="10" max="100" />
-          <progress className="progress" value="10" max="100" />
-          결과 영역
+        <section className="flex flex-col p-4 items-center gap-2">
+          <div className="flex flex-row gap-3 w-full justify-around">
+            <div className="flex flex-col items-center gap-1">
+              <div
+                className="radial-progress"
+                style={{ '--value': 70, '--size': '4rem' }}
+                role="progressbar"
+              >
+                70%
+              </div>
+              <span>E</span>
+            </div>
+            <div className="flex flex-col items-center gap-1">
+              <div
+                className="radial-progress"
+                style={{ '--value': 70, '--size': '4rem' }}
+                role="progressbar"
+              >
+                70%
+              </div>
+              <span>S</span>
+            </div>
+            <div className="flex flex-col items-center gap-1">
+              <div
+                className="radial-progress"
+                style={{ '--value': 70, '--size': '4rem' }}
+                role="progressbar"
+              >
+                70%
+              </div>
+              <span>T</span>
+            </div>
+            <div className="flex flex-col items-center gap-1">
+              <div
+                className="radial-progress"
+                style={{ '--value': 70, '--size': '4rem' }}
+                role="progressbar"
+              >
+                70%
+              </div>
+              <span>J</span>
+            </div>
+          </div>
+
+          {/* <div className="flex w-full gap-2 items-center">
+            <span>E</span>
+            <progress className="progress" value="53" max="100" />
+            <span>I</span>
+          </div>
+          <div className="flex w-full gap-2 items-center">
+            <span>E</span>
+            <progress className="progress" value="42" max="100" />
+            <span>I</span>
+          </div>
+          <div className="flex w-full gap-2 items-center">
+            <span>E</span>
+            <progress className="progress" value="88" max="100" />
+            <span>I</span>
+          </div>
+          <div className="flex w-full gap-2 items-center">
+            <span>E</span>
+            <progress className="progress" value="75" max="100" />
+            <span>I</span>
+          </div> */}
           <h2 className="card-title">너는 엣티제야</h2>
           <h3>검사일자 : 2024-03-22 13:22:49</h3>
-        </div>
-      </div>
+        </section>
+      </section>
       <Pagination />
-    </>
+    </main>
   );
 };
 
