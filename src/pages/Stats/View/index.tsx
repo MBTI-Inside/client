372be7cf75479c055ffc6ce30027fef09e89235a
rebi13 <rebi13@naver.com>
2024-03-27 23:49:27 +0900

    style: 문항별 통계 초안 MBTI-Inside#2

--- src/pages/Stats/View/index.tsx
+++ src/pages/Stats/View/index.tsx
@@ -33,13 +33,26 @@ const StatsView = () => {
         <Badge>내용 : 의자</Badge>
         <Badge>내용 : 상상</Badge>
       </section>
-      <section className="bg-yellow-800">
+      <section className="bg-yellow-500">
         <div>문항 타입 : E</div>
         <div>문항 질문 : 내가 더 잘 알고 있는 것은?</div>
-        <div>차트</div>
-        <div>E답변: 상대방을 기분 좋게 하는 방법 :D</div>
-        <div>I답변: 상대방을 기분 나쁘지 않게 배려하는 방법 :)</div>
-        <div></div>
+        <div>
+          <progress
+            className="progress progress-primary [--progressbg-shdw:yellow] bg-green-300 h-8 "
+            value={30}
+            max={100}
+          />
+        </div>
+        <div className="flex flex-row items-center justify-center gap-4">
+          <Badge className="bg-primary border-0">E</Badge>
+          <div>상대방을 기분 좋게 하는 방법 :D</div>
+          <span>58%</span>
+        </div>
+        <div className="flex flex-row items-center justify-center gap-4">
+          <Badge className="bg-green-300 border-0">I</Badge>
+          <div>상대방을 기분 나쁘지 않게 배려하는 방법 :)</div>
+          <span>42%</span>
+        </div>
       </section>
       <Pagination />
     </div>
