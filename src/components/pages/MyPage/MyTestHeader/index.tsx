66c44093f7ac2bc574b4fe3b8d683f69e8e3c1e6
rebi13 <rebi13@naver.com>
2024-03-27 19:32:28 +0900

    chore: 파일명 변경 MBTI-Inside#8

--- src/components/pages/MyPage/MyTestHeader/index.tsx
+++ src/components/pages/MyPage/MyTestHeader/index.tsx
@@ -6,11 +6,11 @@ import * as S from '@/components/pages/MyPage/MyTestHeader/styles';
 const MyTestHeader = () => {
   return (
     <S.MyTestHeaderContainer>
-      <div className="text-xl text-white">
+      <div className="pl-4 text-xl text-white">
         <IoIosArrowBack onClick={() => {}} />
       </div>
       <span className="font-bold text-xl text-white">내 MBTI</span>
-      <div className="text-xl text-white">
+      <div className="pr-4 text-xl text-white">
         <GrAdobeCreativeCloud />
       </div>
     </S.MyTestHeaderContainer>
