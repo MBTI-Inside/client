6dd23f8220bbaa6422bac38e54b8c67ab6d24416
rebi13 <rebi13@naver.com>
2024-03-30 22:11:16 +0900

    style: 문항별통계 컴포넌트 스타일링 MBTI-Inside#2

--- src/pages/Stats/View/index.tsx
+++ src/pages/Stats/View/index.tsx
@@ -4,10 +4,13 @@ import tw from 'tailwind-styled-components';
 
 import Button from '@/components/common/Button';
 import Pagination from '@/components/common/Pagination';
+import StatsCard from '@/components/pages/Stats/StatsCard';
+
+import * as S from '@/pages/Stats/View/styles';
 
 const StatsView = () => {
   return (
-    <div className="flex flex-col w-full items-center gap-2">
+    <S.StatsViewContainer>
       <MbtiTitleContainer>
         <MbtiTitle>
           {/* TODO: 검색 데이터는 전역 상태 관리 */}
@@ -33,40 +36,9 @@ const StatsView = () => {
         <Badge>내용 : 의자</Badge>
         <Badge>내용 : 상상</Badge>
       </section>
-      <section className="flex flex-col items-center justify-center bg-yellow-500 w-11/12 p-2 gap-2 rounded-2xl">
-        <div className="flex gap-2 w-full">
-          <Badge className="bg-primary border-0 w-1/12">E</Badge>
-          <span>
-            내가 더 잘 알고 있는 것은? 내가 더 잘 알고 있는 것은? 내가 더 잘
-            알고 있는 것은? 내가 더 잘 알고 있는 것은? 내가 더 잘 알고 있는
-            것은?
-          </span>
-        </div>
-        <div className="flex w-11/12 items-center">
-          <progress
-            className="progress progress-primary [--progressbg-shdw:yellow] bg-green-300 h-8 "
-            value={30}
-            max={100}
-          />
-        </div>
-        <div className="flex flex-row items-center justify-center gap-4 w-full">
-          <Badge className="bg-primary border-0 w-1/12">E</Badge>
-          <div className="w-2/3">
-            상대방을 기분 좋게 하는 방법 :D 상대방을 기분 좋게 하는 방법 :D
-            상대방을 기분 좋게 하는 방법 :D{' '}
-          </div>
-          <span className="w-1/6 text-end">58%</span>
-        </div>
-        <div className="flex flex-row items-center justify-center gap-4 w-full">
-          <Badge className="bg-green-300 border-0 w-1/12">I</Badge>
-          <div className="w-2/3">
-            상대방을 기분 나쁘지 않게 배려하는 방법 :)
-          </div>
-          <span className="w-1/6 text-end">42%</span>
-        </div>
-      </section>
+      <StatsCard />
       <Pagination />
-    </div>
+    </S.StatsViewContainer>
   );
 };
 
