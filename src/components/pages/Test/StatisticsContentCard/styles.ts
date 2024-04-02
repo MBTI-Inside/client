a224bebd5cb68af3adceb211d58cb946d70f5a67
rebi13 <rebi13@naver.com>
2024-04-03 00:15:28 +0900

    style: 테스트 검사결과 통계 영역 컴포넌트 스타일링 MBTI-Inside#3

---
+++ src/components/pages/Test/StatisticsContentCard/styles.ts
@@ -0,0 +1,22 @@
+import tw from 'tailwind-styled-components';
+
+export const StatisticsContentCardContainer = tw.section`
+    flex 
+    flex-col 
+    items-center 
+    justify-center
+`;
+
+export const ProgressBar = tw.div`
+    flex 
+    w-full 
+    items-center 
+    justify-center 
+    gap-4
+`;
+
+export const TypesContainer = tw.div`
+    flex 
+    justify-between 
+    w-full
+`;
