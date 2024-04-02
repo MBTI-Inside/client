5ab70820a4e0c51378f305acd3292288611b06b9
rebi13 <rebi13@naver.com>
2024-04-02 23:17:04 +0900

    style: MBTI 결과 설명 영역 컴포넌트 스타일링 MBTI-Inside#3

---
+++ src/components/pages/Test/ResultDescription/index.tsx
@@ -0,0 +1,28 @@
+import * as S from '@/components/pages/Test/ResultDescription/styles';
+
+const ResultDescription = () => {
+  return (
+    <S.ResultDescriptionContainer>
+      <S.Title>리더안하면 병드는 ESTJ</S.Title>
+      <S.Content>
+        ESTJ 유형은 일을 조직하고 프로젝트를 계획하고 출범시키는 능력이
+        뛰어납니다. 현실적이고 사실적이며 체계적, 논리적으로 사업이나 조직체를
+        이끌어가는 타고난 재능을 가졌기 때문입니다. 혼돈스러운 상태나 불분명한
+        상태 또는 실용성이 없는 분야에는 큰 흥미가 없습니다. 그러나 필요시에는
+        언제나 응용하는 힘이 있습니다. 분명한 규칙을 중요시하고 그에 따라
+        행동하고 일을 추진하고 완성해 나갑니다. 어떤 계획이나 결정을 내릴 때
+        확고한 사실에 바탕을 두고 이행합니다. 대체로 결과를 현재 볼 수 있는 일을
+        즐기는 편입니다.
+      </S.Content>
+      <S.BadgeGroup>
+        {/* TODO: 뱃지 공통 컴포넌트 적용 */}
+        <div className="badge">#책임감</div>
+        <div className="badge">#꼰대</div>
+        <div className="badge">#호불호확실</div>
+        <div className="badge">#단계적계획</div>
+      </S.BadgeGroup>
+    </S.ResultDescriptionContainer>
+  );
+};
+
+export default ResultDescription;
---
+++ src/components/pages/Test/ResultDescription/styles.ts
@@ -0,0 +1,29 @@
+import tw from 'tailwind-styled-components';
+
+export const ResultDescriptionContainer = tw.section`
+    flex 
+    flex-col 
+    items-center 
+    justify-center 
+    gap-2 
+    w-5/6
+`;
+
+export const Title = tw.div`
+    text-2xl 
+    font-bold 
+    text-white
+`;
+
+export const Content = tw.div`
+    text-xl 
+    text-white 
+    p-4
+`;
+
+export const BadgeGroup = tw.div`
+    flex 
+    items-center 
+    justify-center 
+    gap-2
+`;
