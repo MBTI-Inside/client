0c0c3d7a8ca34f4f42b20caf35702e5c351f8636
rebi13 <rebi13@naver.com>
2024-04-04 22:45:15 +0900

    style: 테스트 결과 페이지 컴포넌트 스타일링 완료 MBTI-Inside#3

--- src/pages/Test/Result/index.tsx
+++ src/pages/Test/Result/index.tsx
@@ -1,5 +1,3 @@
-import tw from 'tailwind-styled-components';
-
 import Button from '@/components/common/Button';
 import Character from '@/components/common/Character';
 import ResultDescription from '@/components/pages/Test/ResultDescription';
@@ -7,42 +5,29 @@ import ResultHeader from '@/components/pages/Test/ResultHeader';
 import ResultStatistics from '@/components/pages/Test/ResultStatistics';
 import ResultTypesRelations from '@/components/pages/Test/ResultTypesRelations';
 
+import * as S from '@/pages/Test/Result/styles';
+
 const TestResult = () => {
   return (
-    <TestResultContainer style={{ backgroundColor: '#DDAADD' }}>
+    <S.TestResultContainer color={'bg-[#DDAADD]'}>
       <ResultHeader />
       <Character bgcolor={'#AAFFAA'} gcolor={'#DDAADD'} />
       <ResultDescription />
       <ResultStatistics />
       <ResultTypesRelations />
-
-      <Buttons>
-        <Button>다시하기</Button>
-        <Button>통계 보러가기</Button>
-        <Button>담벼락 보러가기</Button>
-      </Buttons>
-    </TestResultContainer>
+      <S.Buttons>
+        <Button classProp="w-full h-12 text-lg bg-black text-white border-none">
+          다시하기
+        </Button>
+        <Button classProp="w-full h-12 text-lg bg-black text-white border-none">
+          통계 보러가기
+        </Button>
+        <Button classProp="w-full h-12 text-lg bg-black text-white border-none">
+          담벼락 보러가기
+        </Button>
+      </S.Buttons>
+    </S.TestResultContainer>
   );
 };
 
 export default TestResult;
-// 테스트 결과를 보여줌
-
-export const TestResultContainer = tw.section`
-flex
-flex-col
-w-full
-
-flex-1
-justify-center
-items-center
-gap-12
-`;
-
-export const Buttons = tw.div`
-flex
-flex-col
-w-5/6
-gap-4
-mb-4
-`;
