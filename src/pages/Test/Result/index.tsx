f1d2774c3176dfe422b1d64a26b853b0ce8c047c
rebi13 <rebi13@naver.com>
2024-04-04 22:38:16 +0900

    style: 유형별 관계성 컴포넌트 스타일링 MBTI-Inside#3

--- src/pages/Test/Result/index.tsx
+++ src/pages/Test/Result/index.tsx
@@ -5,6 +5,7 @@ import Character from '@/components/common/Character';
 import ResultDescription from '@/components/pages/Test/ResultDescription';
 import ResultHeader from '@/components/pages/Test/ResultHeader';
 import ResultStatistics from '@/components/pages/Test/ResultStatistics';
+import ResultTypesRelations from '@/components/pages/Test/ResultTypesRelations';
 
 const TestResult = () => {
   return (
@@ -13,19 +14,8 @@ const TestResult = () => {
       <Character bgcolor={'#AAFFAA'} gcolor={'#DDAADD'} />
       <ResultDescription />
       <ResultStatistics />
-      <GoodBadContainer>
-        <GoodBadTitle>유형별 관계성</GoodBadTitle>
-        <GoodType>
-          <TypeTitle>잘 어울리는 유형</TypeTitle>
-          <Mbti>❤️good?.name❤️</Mbti>
-          <Contents>good?.description</Contents>
-        </GoodType>
-        <BadType>
-          <TypeTitle>친해지기 힘든 유형</TypeTitle>
-          <Mbti>❄️bad?.name❄️</Mbti>
-          <Contents>bad?.description</Contents>
-        </BadType>
-      </GoodBadContainer>
+      <ResultTypesRelations />
+
       <Buttons>
         <Button>다시하기</Button>
         <Button>통계 보러가기</Button>
@@ -55,48 +45,4 @@ flex-col
 w-5/6
 gap-4
 mb-4
-`;
-
-export const GoodBadContainer = tw.div`
-flex
-flex-col
-items-center
-px-4
-text-black
-bg-white
-rounded-xl
-w-5/6
-`;
-export const GoodBadTitle = tw.h5`
-text-3xl
-font-bold
-py-6
-`;
-export const GoodType = tw.div`
-mt-2
-mb-6
-bg-pink-100
-rounded-lg
-p-4
-`;
-export const BadType = tw.div`
-mb-8
-bg-blue-100
-rounded-lg
-p-4
-`;
-export const TypeTitle = tw.p`
-text-lg
-text-center
-font-bold
-mb-2
-`;
-export const Mbti = tw.p`
-text-base
-text-center
-font-bold
-mb-2
-`;
-export const Contents = tw.div`
-w-full
 `;
