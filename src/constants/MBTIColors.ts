2c002fe2e20d9b22ca0400dd73e049b2b505b45b
rebi13 <rebi13@naver.com>
2024-04-13 13:49:40 +0900

    feat: MBTI 요소별, 유형별 색상 정의 MBIT-Inside#19

---
+++ src/constants/MBTIColors.ts
@@ -0,0 +1,29 @@
+export const MBTI_ELEMENT_COLORS = {
+  E: '#F4D03F',
+  I: '#5D6D7E',
+  N: '#7D3C98',
+  S: '#3498DB',
+  T: '#1ABC9C',
+  F: '#E74C3C',
+  J: '#2E4053',
+  P: '#F39C12'
+};
+
+export const MBTI_TYPE_COLORS = {
+  INTJ: '#482E5D',
+  INTP: '#36797D',
+  ENTJ: '#436280',
+  ENTP: '#737B4E',
+  INFJ: '#913838',
+  INFP: '#8E5572',
+  ENFJ: '#A4784E',
+  ENFP: '#D16B4B',
+  ISTJ: '#2E5A78',
+  ISFJ: '#945353',
+  ESTJ: '#5D798D',
+  ESFJ: '#D18B47',
+  ISTP: '#3B8C8A',
+  ISFP: '#806060',
+  ESTP: '#D1A549',
+  ESFP: '#DA7B47'
+};
