96374df3bc1a184e7ec36761cf845de0aea6ec26
rebi13 <rebi13@naver.com>
2024-04-15 22:04:16 +0900

    feature: dropdown 샘플 데이터 적용 MBTI-Inside#32

--- src/components/layout/Header/index.tsx
+++ src/components/layout/Header/index.tsx
@@ -1,5 +1,6 @@
 import useRouter from '@/hooks/useRouter';
 
+import Dropdown from '@/components/common/Dropdown';
 import SideBar from '@/components/common/SideBar';
 
 import MainLogoSvg from '@/assets/image/mainlogo.svg';
@@ -9,6 +10,16 @@ import * as S from './index.styles';
 const Header = () => {
   const { navigateTo } = useRouter();
 
+  const menuItems = [
+    { id: 1, label: 'Profile', onClick: () => console.log('Profile clicked') },
+    {
+      id: 2,
+      label: 'Settings',
+      onClick: () => console.log('Settings clicked')
+    },
+    { id: 3, label: 'Logout', onClick: () => console.log('Logout clicked') }
+  ];
+
   return (
     <S.HeaderContainer>
       <a onClick={() => navigateTo('/')}>
@@ -16,9 +27,12 @@ const Header = () => {
       </a>
       <S.InterfaceContainer>
         {/* 로그인 시 표시*/}
-        <a onClick={() => alert('TODO: 메뉴 보여주기')}>
+        {/* <a onClick={() => alert('TODO: 메뉴 보여주기')}>
           <S.UserIcon />
-        </a>
+        </a> */}
+        <Dropdown items={menuItems}>
+          <S.UserIcon /> {/* 이 아이콘을 클릭하면 Dropdown이 트리거됩니다 */}
+        </Dropdown>
         <a onClick={() => navigateTo('/login')}>
           <S.LoginIcon />
         </a>
