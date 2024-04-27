ade866b406c46214fe2f6014f5e1c528c15a1913
rebi13 <rebi13@naver.com>
2024-04-27 15:20:43 +0900

    style: 메모 상세 뷰 헤더 스타일링 최적화 MBTI-Inside#5

--- src/components/pages/Memo/MemoViewHeader/index.tsx
+++ src/components/pages/Memo/MemoViewHeader/index.tsx
@@ -1,31 +1,40 @@
+import { useMemo } from 'react';
 import { CiMenuKebab } from 'react-icons/ci';
 import { IoIosArrowBack } from 'react-icons/io';
 import { useNavigate } from 'react-router-dom';
 
+import Left from '@/components/layout/Header/Left';
+import Middle from '@/components/layout/Header/Middle';
+import Right from '@/components/layout/Header/Right';
 import * as S from '@/components/pages/Memo/MemoViewHeader/index.styles';
 
 const MemoViewHeader = () => {
   const nav = useNavigate();
 
+  const menuItems = useMemo(() => {
+    const items = [
+      { id: 1, label: '내정보', onClick: () => {} },
+      { id: 2, label: '로그아웃', onClick: () => {} }
+    ];
+    return items;
+  }, []);
+
   return (
     <S.MemoViewHeaderContainer>
-      <div className="pl-4 text-xl">
+      <Left>
         <IoIosArrowBack onClick={() => nav(-1)} />
-      </div>
-      <div className="text-xl">ESTJ</div>
-      <S.DropDowns>
-        <S.DropDownButton tabIndex={0} role="button">
-          <CiMenuKebab />
-        </S.DropDownButton>
-        <S.DropDownContents tabIndex={0}>
-          <li className="items-center">
-            <a>수정</a>
-          </li>
-          <li className="items-center">
-            <a>삭제</a>
-          </li>
-        </S.DropDownContents>
-      </S.DropDowns>
+      </Left>
+      <Middle>ESTJ</Middle>
+      <Right>
+        <CiMenuKebab
+          onClick={() => {
+            console.log('11');
+          }}
+        />
+      </Right>
+      {/* <Dropdown items={menuItems}> */}
+      {/* <S.MenuIcon /> */}
+      {/* </Dropdown> */}
     </S.MemoViewHeaderContainer>
   );
 };
