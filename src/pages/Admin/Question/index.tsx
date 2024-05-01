13aa40664a629561e9659d72c231adb6bf43b4d3
rebi13 <rebi13@naver.com>
2024-05-01 22:38:01 +0900

    style: Admin 문항 샘플 MBTI-Inside#5

--- src/pages/Admin/Question/index.tsx
+++ src/pages/Admin/Question/index.tsx
@@ -1,3 +1,4 @@
+import Badge from '@/components/common/Badge';
 import Pagination from '@/components/common/Pagination';
 import QuestionCard from '@/components/pages/Admin/Question/QuestionCard';
 import QuestionHeader from '@/components/pages/Admin/Question/QuestionHeader';
@@ -8,6 +9,35 @@ const QuestionList = () => {
   return (
     <S.QuestionContainer>
       <QuestionHeader />
+      <section className="w-5/6">
+        <div className="bg-white">
+          <div className="flex">
+            <Badge content="energy" />
+            <div className="line-clamp-2">
+              바쁜 회사 생활을 보낸 당신. 황금 같은 주말을 어떻게 보내려고 할까?
+            </div>
+          </div>
+          <div>
+            <div
+              className="    flex 
+    w-full
+    items-center"
+            >
+              <div className="badge">E</div>
+              <span className="w-1/6 text-left">72%</span>
+              <progress
+                className="progress bg-slate-400"
+                value={72}
+                max={100}
+              />
+              <span className="w-1/6 text-right">28%</span>
+              <div className="badge">I</div>
+            </div>
+          </div>
+        </div>
+
+        <div></div>
+      </section>
       <S.QuestionSection>
         <QuestionCard />
         <QuestionCard />
