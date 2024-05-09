08f96dc0c5532a0059939e812521cdf17f200461
rebi13 <rebi13@naver.com>
2024-05-10 00:24:30 +0900

    style: Admin 문항 단건 조회 페이지 디자인 초안 MBTI-Inside#5

--- src/components/pages/Admin/Question/QuestionViewCard/index.tsx
+++ src/components/pages/Admin/Question/QuestionViewCard/index.tsx
@@ -8,6 +8,24 @@ const QuestionViewCard = () => {
           친구와 1시간이 넘는 통화를 마친 뒤 당신의 상태는?
         </S.TestQuestion>
       </section>
+      <S.QuestionType>
+        <div className="font-bold text-2xl">문항 유형</div>
+        <div className="font-bold text-2xl">energy</div>
+      </S.QuestionType>
+      <S.AnswerSection>
+        <S.AnswerCard>
+          E 75% <br /> 남은 얘기는 만나서 해야징
+        </S.AnswerCard>
+        <S.AnswerCard>
+          I 25% <br /> 통화가 끝났으니 이제 쉬어야지..
+        </S.AnswerCard>
+      </S.AnswerSection>
+
+      {/* <section>
+        <S.TestQuestion>
+          친구와 1시간이 넘는 통화를 마친 뒤 당신의 상태는?
+        </S.TestQuestion>
+      </section>
       <S.AnswerSection>
         <S.AnswerCard>남은 얘기는 만나서 해야징</S.AnswerCard>
         <S.AnswerCard>통화가 끝났으니 이제 쉬어야지..</S.AnswerCard>
@@ -20,7 +38,7 @@ const QuestionViewCard = () => {
         <div className="font-bold text-lg">비중도</div>
         <S.Progress value="73" max="100"></S.Progress>
         <div className="font-bold text-lg">73%</div>
-      </S.TypeRate>
+      </S.TypeRate> */}
     </S.QuestionViewCardContainer>
   );
 };
