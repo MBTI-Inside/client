d976522a6a1e6560266918556672a4f5ec3e5f11
rebi13 <rebi13@naver.com>
2024-05-09 23:56:06 +0900

    feature: 뱃지 X버튼 여부 추가 MBTI-Inside#5

--- src/components/common/Badge/index.tsx
+++ src/components/common/Badge/index.tsx
@@ -3,13 +3,14 @@ import Button from '@/components/common/Button';
 
 export interface BadgeProps {
   content: string;
+  isClose?: boolean;
 }
 
-const Badge = ({ content }: BadgeProps) => {
+const Badge = ({ content, isClose = true }: BadgeProps) => {
   return (
     <S.BadgeContainer>
       {content}
-      <Button classProp="btn-xs w-4 h-4 bg-secondary">X</Button>
+      {isClose && <Button classProp="btn-xs w-4 h-4 bg-secondary">X</Button>}
     </S.BadgeContainer>
   );
 };
