d646a3d220fb355271c578bcfdb21933834e9f9e
rebi13 <rebi13@naver.com>
2024-05-11 22:45:19 +0900

    style:mypage 메모 목록 스타일 최적화 MBTI-Inside#5

--- src/pages/MyPage/PostList/index.tsx
+++ src/pages/MyPage/PostList/index.tsx
@@ -6,6 +6,7 @@ import * as S from '@/pages/MyPage/PostList/styles';
 const MyPostList = () => {
   // 임시 데이터
   const a = {
+    id: 236547,
     title: 'Lorem Ipsum is simply dummy text of the printing',
     content:
       'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
@@ -20,10 +21,10 @@ const MyPostList = () => {
       <MyHeader title={'내 MemoBTI'} />
       {/* TODO: 무한스크롤 */}
       <S.MyCardContainer>
-        <Card {...a} />
-        <Card {...a} />
-        <Card {...a} />
-        <Card {...a} />
+        <Card memo={a} />
+        <Card memo={a} />
+        <Card memo={a} />
+        <Card memo={a} />
       </S.MyCardContainer>
     </S.MyPostListContainer>
   );
