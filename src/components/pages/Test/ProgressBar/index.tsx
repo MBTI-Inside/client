0ee714f865eb1b0d5723311d93a9727964417139
rebi13 <rebi13@naver.com>
2024-05-15 23:49:30 +0900

    style: 테스트 페이지 스타일링 최적화 MBTI-Inside#5

--- src/components/pages/Test/ProgressBar/index.tsx
+++ src/components/pages/Test/ProgressBar/index.tsx
@@ -3,8 +3,12 @@ import * as S from '@/components/pages/Test/ProgressBar/styles';
 const ProgressBar = () => {
   return (
     <S.ProgressBarContainer>
-      <div className="text-end">1/10</div>
-      <progress className="progress" value="10" max="100"></progress>
+      <div className="text-end text-white">1 / 16</div>
+      <progress
+        className="progress progress-info bg-white border-none"
+        value="10"
+        max="100"
+      ></progress>
     </S.ProgressBarContainer>
   );
 };
