504c528953b13b197d2a054d0619b7182ffdbd50
rebi13 <rebi13@naver.com>
2024-05-24 23:33:00 +0900

    style: 헤더 영역 스타일링 최적화 MBTI-Inside#5

---
+++ src/components/pages/Stats/StatsHeader/styles.ts
@@ -0,0 +1,28 @@
+import tw from 'tailwind-styled-components';
+
+export const StatsTitleContainer = tw.div`
+  flex 
+  flex-col 
+  items-center 
+  p-3
+  justify-between
+  gap-2
+  w-full
+`;
+
+export const StatsTitle = tw.div`
+  flex 
+  w-full 
+  justify-between
+`;
+
+export const Title = tw.div`
+  text-4xl
+  font-bold
+  text-white
+`;
+
+export const ButtonGroup = tw.div`
+  flex 
+  gap-2
+`;
--- src/pages/Stats/View/index.tsx
+++ src/pages/Stats/View/index.tsx
@@ -1,41 +1,15 @@
-import { FaExchangeAlt } from 'react-icons/fa';
-import { PiMagnifyingGlassBold } from 'react-icons/pi';
 import tw from 'tailwind-styled-components';
 
-import Button from '@/components/common/Button';
 import Pagination from '@/components/common/Pagination';
 import StatsCard from '@/components/pages/Stats/StatsCard';
+import StatsHeader from '@/components/pages/Stats/StatsHeader';
 
 import * as S from '@/pages/Stats/View/styles';
 
 const StatsView = () => {
   return (
     <S.StatsViewContainer>
-      <MbtiTitleContainer>
-        <MbtiTitle>
-          {/* TODO: 검색 데이터는 전역 상태 관리 */}
-          <Title>StatsBTI</Title>
-          <div>
-            {/* 임시 */}
-            <Button>
-              <PiMagnifyingGlassBold />
-            </Button>
-            <Button>
-              <FaExchangeAlt />
-            </Button>
-          </div>
-        </MbtiTitle>
-      </MbtiTitleContainer>
-      <section className="bg-white">
-        <Badge>E</Badge>
-        <Badge>I</Badge>
-        <Badge>제목 : 진짜</Badge>
-        <Badge>내용 : 진짜</Badge>
-        <Badge>내용 : 버스에서</Badge>
-        <Badge>내용 : 창밖</Badge>
-        <Badge>내용 : 의자</Badge>
-        <Badge>내용 : 상상</Badge>
-      </section>
+      <StatsHeader />
       <StatsCard />
       <Pagination />
     </S.StatsViewContainer>
