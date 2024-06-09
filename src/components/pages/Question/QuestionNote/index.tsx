991af822da328a16b5cfee9787fde8f36158f175
rebi13 <rebi13@naver.com>
2024-06-09 22:46:17 +0900

    feature: 문항 선택, 작성, 비중도 data 세팅 MBTI-Inside/todo#4

--- src/components/pages/Question/QuestionNote/index.tsx
+++ src/components/pages/Question/QuestionNote/index.tsx
@@ -1,16 +1,38 @@
-import { useRef, useState } from 'react';
+import { MBTI_TYPES_OPTIONS } from '@/constants';
+import { useEffect, useRef, useState } from 'react';
 
 import Button from '@/components/common/Button';
 import * as S from '@/components/pages/Question/QuestionNote/styles';
 
+import { MBTI_OPTIONS_DATA } from '@/constants/MBTIOptions';
+
 const QuestionNote = (params: any) => {
   const { id, question, answerTop, answerBottom } = params;
+  const text = '123asdf';
+  console.log(question, answerTop, answerBottom);
 
-  const questionRef = useRef(question);
-  const answerTopRef = useRef(answerTop);
-  const answerBottomRef = useRef(answerBottom);
+  const [mbtiTypesOption, setMBTITypesOption] =
+    useState<keyof typeof MBTI_OPTIONS_DATA>('Energy');
+
+  const [mbtiTypes, setMBTITypes] = useState<string[]>(
+    MBTI_OPTIONS_DATA[mbtiTypesOption]
+  );
+  const questionRef = useRef<HTMLInputElement>(null);
+  const answerTopRef = useRef<HTMLTextAreaElement>(null);
+  const answerBottomRef = useRef<HTMLTextAreaElement>(null);
 
   const [rangeValue, setRangeValue] = useState(50);
+
+  useEffect(() => {
+    setMBTITypes(MBTI_OPTIONS_DATA[mbtiTypesOption]);
+  }, [mbtiTypesOption]);
+
+  const saveQuestion = () => {
+    console.log('제목 : ' + questionRef.current?.value);
+    console.log('답변1 : ' + answerTopRef.current?.value);
+    console.log('답변2 : ' + answerBottomRef.current?.value);
+    console.log('비중도 : ' + rangeValue);
+  };
 
   return (
     <S.NoteContainer>
@@ -18,29 +40,37 @@ const QuestionNote = (params: any) => {
         <div className="label">
           <span className="label-text text-white">문항유형</span>
         </div>
-        <select className="select select-bordered w-full">
-          <option>energy</option>
-          <option>awareness</option>
-          <option>judgement</option>
-          <option>life</option>
+        <select
+          className="select select-bordered w-full"
+          onChange={(e) => {
+            setMBTITypesOption(
+              e.target.value as keyof typeof MBTI_OPTIONS_DATA
+            );
+          }}
+        >
+          {MBTI_TYPES_OPTIONS.map((option) => (
+            <option key={option} value={option}>
+              {option}
+            </option>
+          ))}
         </select>
       </section>
       <S.InputTitle
         type="text"
         placeholder="제목을 입력하세요."
-        defaultValue={questionRef.current}
+        value={question}
         ref={questionRef}
       />
       <S.InputContent
         className="textarea-primary"
         placeholder="답변1"
-        defaultValue={answerTopRef.current}
+        value={answerTop}
         ref={answerTopRef}
       />
       <S.InputContent
         className="textarea-secondary"
         placeholder="답변2"
-        defaultValue={answerBottomRef.current}
+        value={answerBottom}
         ref={answerBottomRef}
       />
       <section className="flex flex-col gap-2">
@@ -48,7 +78,7 @@ const QuestionNote = (params: any) => {
           <span className="label-text text-white">비중도</span>
         </div>
         <div className="flex justify-between items-center bg-white p-2 gap-2 rounded-full">
-          <span className="w-2">E</span>
+          <span className="w-2">{mbtiTypes[0]}</span>
           <span className="flex items-center w-full">
             <input
               type="range"
@@ -61,27 +91,32 @@ const QuestionNote = (params: any) => {
               onChange={(e) => setRangeValue(Number(e.target.value))}
             />
           </span>
-          <span>50%</span>
+          <span className="w-8">{rangeValue}%</span>
         </div>
         <div className="flex justify-between items-center bg-white p-2 gap-2 rounded-full">
-          <span className="w-2">I</span>
+          <span className="w-2">{mbtiTypes[1]}</span>
           <span className="flex items-center w-full">
             <input
               type="range"
               min="0"
               max="100"
-              value={rangeValue}
+              value={100 - rangeValue}
               className="range range-secondary"
               style={{ backgroundColor: 'none' }}
               step="1"
               onChange={(e) => setRangeValue(Number(e.target.value))}
             />
           </span>
-          <span>50%</span>
+          <span className="w-8">{100 - rangeValue}%</span>
         </div>
       </section>
 
-      <Button classProp={'w-full h-14 text-lg text-white bg-accent'}>
+      <Button
+        classProp="w-full h-14 text-lg text-white bg-accent"
+        onClick={() => {
+          saveQuestion();
+        }}
+      >
         작성 완료
       </Button>
     </S.NoteContainer>
