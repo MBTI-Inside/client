cd803270673b6972ae563fe7294617ca0364473f
rebi13 <rebi13@naver.com>
2024-06-17 23:39:34 +0900

    fix: api 요청에 맞도록 대소문자 구분 MBTI-Inside/todo#4

--- src/constants/MBTIOptions.ts
+++ src/constants/MBTIOptions.ts
@@ -1,10 +1,10 @@
 type MBTIElementOption = 'E' | 'I' | 'N' | 'S' | 'T' | 'F' | 'J' | 'P';
-type MBTITypesOption = 'Energy' | 'Awareness' | 'Judgement' | 'Life';
+type MBTITypesOption = 'energy' | 'awareness' | 'judgement' | 'life';
 type MBTIDatasOption = {
-  Energy: string[];
-  Awareness: string[];
-  Judgement: string[];
-  Life: string[];
+  energy: string[];
+  awareness: string[];
+  judgement: string[];
+  life: string[];
 };
 
 export const MBTI_ELEMENT_OPTIONS: MBTIElementOption[] = [
@@ -19,15 +19,15 @@ export const MBTI_ELEMENT_OPTIONS: MBTIElementOption[] = [
 ];
 
 export const MBTI_TYPES_OPTIONS: MBTITypesOption[] = [
-  'Energy',
-  'Awareness',
-  'Judgement',
-  'Life'
+  'energy',
+  'awareness',
+  'judgement',
+  'life'
 ];
 
 export const MBTI_OPTIONS_DATA: MBTIDatasOption = {
-  Energy: ['E', 'I'],
-  Awareness: ['N', 'S'],
-  Judgement: ['T', 'F'],
-  Life: ['P', 'J']
+  energy: ['E', 'I'],
+  awareness: ['N', 'S'],
+  judgement: ['T', 'F'],
+  life: ['P', 'J']
 };
