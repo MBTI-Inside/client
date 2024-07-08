c6fb336445ef970fac601bba8a0445cdd0ffaab4
rebi13 <rebi13@naver.com>
2024-07-08 23:29:13 +0900

    refactor: tanstack query 커스텀 훅 MBTI-Inside/todo#10

--- src/hooks/useCustomMutation.ts
+++ src/hooks/useCustomMutation.ts
@@ -1,15 +1,20 @@
-import { useMutation, useQueryClient } from '@tanstack/react-query';
+import {
+  UseMutationResult,
+  useMutation,
+  useQueryClient
+} from '@tanstack/react-query';
+
 import axiosRequest from '../api';
 
-interface infoProps {
-  method: string;
+interface InfoProps {
+  method: 'post' | 'put' | 'delete' | 'patch';
   url: string;
-  data?: {};
-  headers?: {};
+  data?: Record<string, unknown>;
+  headers?: Record<string, string>;
 }
 
 /**
- * 작성자명   : 원종석
+ * 작성자명   : ㅇㅇㅇ
  * 작성일자   : 2023.11.07.(화)
  * 작성내용   : useMutation 커스텀 훅
  * 수정일자   : none
@@ -18,20 +23,23 @@ interface infoProps {
  * @param info 호출 method와 url 정보, body params를 담은 객체 데이터
  * @returns
  */
-const useCustomMutation = <T>(key: string[], info: infoProps) => {
+const useCustomMutation = <T>(
+  key: string[],
+  info: InfoProps
+): UseMutationResult<T, Error, unknown, unknown> => {
   const queryClient = useQueryClient();
 
-  const customMutation = useMutation(
-    () => {
-      return axiosRequest.requestAxios<T>(info.method, info.url, info.data);
-    },
-    {
-      onSuccess: () => {
-        // 새로운 데이터가 서버에 성공적으로 전송되고 새로운 게시물이 추가되었음을 나타내기 위해 사용
-        queryClient.invalidateQueries(key);
-      }
+  const mutationFn = () => {
+    const { method, url, data, headers } = info;
+    return axiosRequest.requestAxios<T>(method, url, data, headers);
+  };
+
+  const customMutation = useMutation<T, Error, unknown, unknown>(mutationFn, {
+    onSuccess: () => {
+      // 새로운 데이터가 서버에 성공적으로 전송되고 새로운 게시물이 추가되었음을 나타내기 위해 사용
+      queryClient.invalidateQueries(key);
     }
-  );
+  });
 
   return customMutation;
 };
--- src/hooks/useCustomQuery.ts
+++ src/hooks/useCustomQuery.ts
@@ -1,15 +1,19 @@
-import { useQuery } from '@tanstack/react-query';
-import axiosRequest from '../api';
+import axiosRequest from '@/api';
+import {
+  UseQueryOptions,
+  UseQueryResult,
+  useQuery
+} from '@tanstack/react-query';
 
 interface QueryData<T> {
   data: T | undefined;
   isLoading: boolean;
   isError: boolean;
-  error: any; // Todo: error-boundary
+  error: Error | null; // Todo: error-boundary
 }
 
-interface infoProps {
-  method: string;
+interface InfoProps {
+  method: 'get';
   url: string;
   enabled: boolean;
 }
@@ -18,18 +22,25 @@ interface infoProps {
  * 작성자명   : 원종석
  * 작성일자   : 2023.11.06.(월)
  * 작성내용   : useQuery 커스텀 훅
- * 수정일자   : none
+ * 수정일자   : 2024.07.08 (월)
  * 수정내용   : none
  * @param key 내부적으로 데이터를 캐시하고 쿼리에 대한 종속성이 변경될 때 자동으로 다시 가져올 수 있게 하는 고유한 키 값
  * @param info 호출 method와 url 정보를 담은 객체 데이터
  * @returns
  */
-const useCustomQuery = <T>(key: string[], info: infoProps): QueryData<T> => {
-  const { data, isLoading, isError, error } = useQuery<T, Error>({
+const useCustomQuery = <T>(key: string[], info: InfoProps): QueryData<T> => {
+  const { method, url, enabled = true } = info;
+
+  const queryFn = () => axiosRequest.requestAxios<T>(method, url);
+
+  const queryOptions: UseQueryOptions<T, Error> = {
     queryKey: key,
-    queryFn: () => axiosRequest.requestAxios<T>(info.method, info.url),
-    enabled: !!info.enabled
-  });
+    queryFn,
+    enabled
+  };
+
+  const { data, isLoading, isError, error }: UseQueryResult<T, Error> =
+    useQuery(queryOptions);
 
   return { data, isLoading, isError, error };
 };
