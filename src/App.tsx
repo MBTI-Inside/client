c81bc5715970dfd8d6e299a09d4ce05bb0b78ce1
rebi13 <rebi13@naver.com>
2024-09-04 23:28:46 +0900

    feature: Error Falback 컴포넌트 MBTI-Inside/todo#8

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import tw from 'tailwind-styled-components';
 
 import ModalProvider from '@/hooks/useModal';
+import useRouter from '@/hooks/useRouter';
 
 import Button from '@/components/common/Button';
 import Loading from '@/components/common/Loading';
@@ -13,13 +14,25 @@ import Header from '@/components/layout/Header';
 
 // TODO: ErrorFallback 컴포넌트 구현
 const ErrorFallback = ({ error, resetErrorBoundary }: FallbackProps) => {
+  const { navigateTo } = useRouter();
   return (
-    <div className="flex flex-col items-center justify-center px-3 pt-52">
-      <section className="text-sky-400">{error.message}</section>
-      <Button onClick={() => resetErrorBoundary()}>reset</Button>
-      <section className="mt-4">
-        <strong className="text-sky-400">잠시 기다려주세요</strong>
-      </section>
+    <div className="card bg-base-100 w-5/6 shadow-xl absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+      <div className="card-body justify-center items-center gap-2">
+        <h2 className="card-title">{error.message}</h2>
+        <p>오류가 발생하였습니다.</p>
+        <p>다시 시도해 주세요.</p>
+        <div className="card-actions justify-end">
+          <Button
+            classProp="btn-primary"
+            onClick={() => {
+              resetErrorBoundary();
+              navigateTo('/');
+            }}
+          >
+            메인 페이지 가기
+          </Button>
+        </div>
+      </div>
     </div>
   );
 };
