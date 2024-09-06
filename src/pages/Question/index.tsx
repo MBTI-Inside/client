9b7b1ed65c44d43141d35364ed8d2558d8596e29
rebi13 <rebi13@naver.com>
2024-09-06 20:07:22 +0900

    feature: 문항 목록 무한스크롤 구현 MBTI-Inside/todo#4

--- src/pages/Question/index.tsx
+++ src/pages/Question/index.tsx
@@ -1,9 +1,13 @@
 import { Question } from '@/@types';
 import axiosRequest from '@/api';
 import { useCustomQuery } from '@/hooks';
+import { useInfiniteQuery } from '@tanstack/react-query';
+import { useState } from 'react';
+import { useInView } from 'react-intersection-observer';
 
 import { useHandleError } from '@/hooks/useHandleError';
 
+import Loading from '@/components/common/Loading';
 import QuestionCard from '@/components/pages/Question/QuestionCard';
 import QuestionHeader from '@/components/pages/Question/QuestionHeader';
 
@@ -16,39 +20,63 @@ export interface QuestionProps {
 const QuestionList = () => {
   const setError = useHandleError(); // 에러 핸들링 함수
 
-  const { data: questions } = useCustomQuery<Question[]>(['get-surveys'], {
-    method: 'get',
-    url: '/surveys',
-    queryFn: () => axiosRequest.requestAxios<Question[]>('get', '/surveys'), // 함수를 반환하도록 수정
-    options: {
-      onError: (error) => {
-        setError(error); // 에러가 발생하면 전역 에러 상태로 설정
+  const [limit] = useState(5);
+  const [skip, setSkip] = useState(0);
+
+  const {
+    data: questions,
+    fetchNextPage,
+    hasNextPage,
+    isFetchingNextPage,
+    error
+  } = useInfiniteQuery(
+    ['get-surveys'],
+    () =>
+      axiosRequest.requestAxios<Question[]>(
+        'get',
+        `/surveys?limit=${limit}&skip=${skip}`
+      ),
+    {
+      getNextPageParam: (lastPage, allPages) => {
+        const totalFetchedItems = allPages.reduce(
+          (acc, page) => acc + page.length,
+          0
+        );
+        // 만약 서버에서 응답받은 데이터가 limit보다 적으면 다음 페이지가 없다고 판단
+        return lastPage.length === limit ? totalFetchedItems : undefined;
+      },
+      refetchOnMount: false, // 컴포넌트가 다시 마운트될 때 데이터를 다시 요청하지 않음
+      refetchOnWindowFocus: false, // 창에 포커스가 다시 맞춰질 때 데이터를 다시 요청하지 않음
+      onError: (error: Error) => {
+        setError(error);
       }
     }
+  );
+
+  const { ref, inView } = useInView({
+    threshold: 1.0,
+    triggerOnce: false
   });
 
-  // const getQuestionList = async () => {
-  //   try {
-  //     const response = await axiosRequest.requestAxios<Question[]>(
-  //       'get',
-  //       '/surveys' //?skip=0&limit=100' // sortType=desc&sortField=id
-  //     );
-  //     setQuestions(response);
-  //   } catch (e) {
-  //     console.log(e);
-  //     throw e;
-  //   }
-  // };
+  // 요소가 뷰포트에 보이면 fetchNextPage 호출
+  if (inView && hasNextPage && !isFetchingNextPage) {
+    setSkip((prev) => prev + 5);
+    fetchNextPage();
+  }
 
-  // TODO: 무한스크롤
   return (
     <S.QuestionContainer>
       <QuestionHeader />
-      {questions?.map((question) => {
-        return <QuestionCard key={question.createdAt} question={question} />;
+      {questions?.pages.map((page, pageIndex) => {
+        return (
+          <div key={pageIndex} className="flex flex-col gap-2 w-full">
+            {page.map((data) => {
+              return <QuestionCard key={data._id} question={data} />;
+            })}
+          </div>
+        );
       })}
-      {/* TODO: Pagination 제거, 무한스크롤 적용 */}
-      {/* <Pagination /> */}
+      <div ref={ref}>{isFetchingNextPage && <Loading />}</div>
     </S.QuestionContainer>
   );
 };
