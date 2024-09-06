ff1c89c3b5c3c52a4a478d8539ddc07ea105ba34
rebi13 <rebi13@naver.com>
2024-09-06 20:39:25 +0900

    style: 디자인 간단하게 표시 MBTI-Inside/todo#4

--- src/components/pages/Question/QuestionViewHeader/index.tsx
+++ src/components/pages/Question/QuestionViewHeader/index.tsx
@@ -28,36 +28,7 @@ const QuestionViewHeader = ({ id }: QuestionViewHeaderProps) => {
         />
       </div>
       <span className="font-bold text-xl">Question</span>
-      <S.DropDowns>
-        <S.DropDownButton tabIndex={0} role="button">
-          <CiMenuKebab />
-        </S.DropDownButton>
-        <S.DropDownContents tabIndex={0}>
-          {/* TODO: 문항 수정 클릭 시 메뉴 닫히도록 적용, 수정할 데이터 조회 */}
-          <li className="items-center">
-            <a
-              onClick={() => {
-                openModal(<QuestionNote id={id} />, null, '문항 수정');
-              }}
-            >
-              수정
-            </a>
-          </li>
-          <li className="items-center">
-            <a
-              onClick={async () => {
-                const response = await axiosRequest.requestAxios<Question>(
-                  'delete',
-                  `/survey/questions/${id}` // sortType=desc&sortField=id
-                );
-                console.log(response);
-              }}
-            >
-              삭제
-            </a>
-          </li>
-        </S.DropDownContents>
-      </S.DropDowns>
+      <span className="pr-4 text-xl">&nbsp;</span>
     </S.QuestionViewHeaderContainer>
   );
 };
