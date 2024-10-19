d02198105390207b8146d02373eb519b201a7ba4
rebi13 <rebi13@naver.com>
2024-10-19 22:49:05 +0900

    chore: todo 주석 제거 MBTI-Inside/todo#5

--- src/components/pages/Test/ResultHeader/index.tsx
+++ src/components/pages/Test/ResultHeader/index.tsx
@@ -36,9 +36,7 @@ const ResultHeader = () => {
 
   return (
     <S.ResultHeaderContainer>
-      {/* TODO: MBTI Type props로 받기 */}
       <p className="font-bold text-5xl text-white">ESTJ</p>
-      {/* TODO: share link */}
       <Button onClick={handleShareClick}>결과 공유하기</Button>
     </S.ResultHeaderContainer>
   );
