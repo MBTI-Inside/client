0b955af6239779b20b21f7ff648b6cc5db5ad80a
rebi13 <rebi13@naver.com>
2024-10-20 23:44:26 +0900

    feature: MBTI 유형별 통계 데이터 바인딩 MBTI-Inside/todo#13

--- src/components/pages/Test/ResultStatistics/index.tsx
+++ src/components/pages/Test/ResultStatistics/index.tsx
@@ -1,14 +1,20 @@
+import { MBTIProportions } from '@/@types';
+
 import * as S from '@/components/pages/Test/ResultStatistics/styles';
 import StatisticsContentCard from '@/components/pages/Test/StatisticsContentCard';
 
-const ResultStatistics = () => {
+interface ResultStatisticsProps {
+  stats: MBTIProportions;
+}
+
+const ResultStatistics = ({ stats }: ResultStatisticsProps) => {
   return (
     <S.ResultStatisticsContainer>
       <S.Title>내 검사 결과</S.Title>
-      <StatisticsContentCard />
-      <StatisticsContentCard />
-      <StatisticsContentCard />
-      <StatisticsContentCard />
+      <StatisticsContentCard stat={stats.energy} mbtiType="energy" />
+      <StatisticsContentCard stat={stats.awareness} mbtiType="awareness" />
+      <StatisticsContentCard stat={stats.judgement} mbtiType="judgement" />
+      <StatisticsContentCard stat={stats.life} mbtiType="life" />
     </S.ResultStatisticsContainer>
   );
 };
--- src/pages/Test/Result/index.tsx
+++ src/pages/Test/Result/index.tsx
@@ -1,4 +1,4 @@
-import { MBTI } from '@/@types';
+import { MBTI, MBTIProportion, MBTIProportions } from '@/@types';
 import axiosRequest from '@/api';
 import { useCustomQuery } from '@/hooks';
 
@@ -18,6 +18,42 @@ import {
   MBTI_TYPE_COLORS_PAIRS
 } from '@/constants/MBTIColors';
 
+// 주어진 배열의 rate 합이 100이 되도록 조정하는 함수
+const normalizeRates = (array: MBTIProportion[]): MBTIProportion[] => {
+  const total = array.reduce((sum, item) => sum + item.rate, 0); // 총합 계산
+
+  // 각 항목의 정수 비율을 계산하고 반올림
+  let adjustedRates = array.map((item) => ({
+    ...item,
+    rate: Math.round((item.rate / total) * 100)
+  }));
+
+  // 정수 합계를 계산하여 100과의 차이를 구함
+  const difference =
+    100 - adjustedRates.reduce((sum, item) => sum + item.rate, 0);
+
+  // 차이가 있다면, 가장 큰 비율 항목에 차이를 더해 보정
+  if (difference !== 0) {
+    const maxItem = adjustedRates.reduce((prev, curr) =>
+      curr.rate > prev.rate ? curr : prev
+    );
+    maxItem.rate += difference;
+  }
+
+  return adjustedRates;
+};
+
+// 모든 키에 대해 rate를 조정하는 함수
+const normalizeStateRates = (state: MBTIProportions): MBTIProportions => {
+  const normalizedState: MBTIProportions = {
+    energy: normalizeRates(state.energy),
+    awareness: normalizeRates(state.awareness),
+    judgement: normalizeRates(state.judgement),
+    life: normalizeRates(state.life)
+  };
+  return normalizedState;
+};
+
 const TestResult = () => {
   const { navigateTo, params, location } = useRouter();
 
@@ -34,6 +70,11 @@ const TestResult = () => {
     }
   });
 
+  // 실행
+  const normalizedState = normalizeStateRates(
+    location.state as MBTIProportions
+  );
+
   return (
     <S.TestResultContainer color={`bg-[${gColor}]`}>
       {/* TODO: mbti 데이터 undefined일 경우 예외처리 / 임시로 'XXXX' 추가 */}
@@ -46,7 +87,7 @@ const TestResult = () => {
           tags: mbtiData?.tags
         }}
       />
-      <ResultStatistics />
+      <ResultStatistics stats={normalizedState} />
       <ResultTypesRelations fits={mbtiData?.fit} />
       <S.Buttons>
         <Button
