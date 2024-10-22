90e271feaf1bd9e86ffa1fe033f176624c9e6bb4
rebi13 <rebi13@naver.com>
2024-10-22 23:06:45 +0900

    feature: 메모 조회 로직 추가 MBTI-Inside/todo#2

--- src/pages/Memo/index.tsx
+++ src/pages/Memo/index.tsx
@@ -1,31 +1,81 @@
+import { MemoPost } from '@/@types';
+import axiosRequest from '@/api';
+import { useInfiniteQuery } from '@tanstack/react-query';
+import { useState } from 'react';
+import { useInView } from 'react-intersection-observer';
+
+import { useHandleError } from '@/hooks/useHandleError';
+
+import Loading from '@/components/common/Loading';
 import Card from '@/components/pages/Memo/Card';
 import MemoHeader from '@/components/pages/Memo/MemoHeader';
 
 import * as S from '@/pages/Memo/styles';
 
 const Memo = () => {
-  const memo = {
-    id: 134178,
-    title: 'Lorem Ipsum is simply dummy text of the printing',
-    content:
-      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
-    date: '3일 전',
-    mbti: 'ESTJ',
-    likeCount: 30,
-    cmtCount: 2
-  };
+  const setError = useHandleError(); // 에러 핸들링 함수
+
+  const [limit] = useState(5);
+  const [skip, setSkip] = useState(0);
+
+  const {
+    data: memos,
+    fetchNextPage,
+    hasNextPage,
+    isFetchingNextPage,
+    error
+  } = useInfiniteQuery(
+    ['get-memos'],
+    () =>
+      axiosRequest.requestAxios<MemoPost[]>(
+        'get',
+        `/memos?limit=${limit}&skip=${skip}`
+      ),
+    {
+      getNextPageParam: (lastPage, allPages) => {
+        const totalFetchedItems = allPages.reduce(
+          (acc, page) => acc + page.length,
+          0
+        );
+        // 만약 서버에서 응답받은 데이터가 limit보다 적으면 다음 페이지가 없다고 판단
+        return lastPage.length === limit ? totalFetchedItems : undefined;
+      },
+      refetchOnMount: false, // 컴포넌트가 다시 마운트될 때 데이터를 다시 요청하지 않음
+      refetchOnWindowFocus: false, // 창에 포커스가 다시 맞춰질 때 데이터를 다시 요청하지 않음
+      onError: (error: Error) => {
+        setError(error);
+      }
+    }
+  );
+
+  const { ref, inView } = useInView({
+    threshold: 1.0,
+    triggerOnce: false
+  });
+
+  // 요소가 뷰포트에 보이면 fetchNextPage 호출
+  if (inView && hasNextPage && !isFetchingNextPage) {
+    setSkip((prev) => prev + 5);
+    fetchNextPage();
+  }
+  console.log(memos);
 
   return (
     <S.MemoContainer>
       <MemoHeader />
-      <Card memo={memo} />
-      <Card memo={memo} />
-      <Card memo={memo} />
-      <Card memo={memo} />
-      <Card memo={memo} />
-      <Card memo={memo} />
-      <Card memo={memo} />
-      <Card memo={memo} />
+      {memos?.pages.map((page, pageIndex) => {
+        return (
+          <div
+            key={pageIndex}
+            className="flex flex-col gap-2 w-full justify-center items-center"
+          >
+            {page.map((data) => {
+              return <Card key={data._id} memo={data} />;
+            })}
+          </div>
+        );
+      })}
+      <div ref={ref}>{isFetchingNextPage && <Loading />}</div>
     </S.MemoContainer>
   );
 };
