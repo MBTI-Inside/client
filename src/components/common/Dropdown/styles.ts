c63c616d3b77360b20d595d592423fd0db1f9e79
rebi13 <rebi13@naver.com>
2024-11-24 00:45:08 +0900

    feature: 드롭다운 스타일 조정 MBTI-Inside/client#32

--- src/components/common/Dropdown/styles.ts
+++ src/components/common/Dropdown/styles.ts
@@ -4,18 +4,26 @@ export const DropdownMenu = tw.section`
     absolute 
     mt-2 
     z-50 
-    w-auto 
+    w-max 
     rounded-md 
     bg-white 
     shadow-lg 
     border 
     border-gray-200
+    right-4 // 오른쪽에서 약간 안쪽으로 여백
+    min-w-[80px] // 최소 너비 설정
+    max-w-[calc(100vw-40px)] // 화면 끝에서 약간 안쪽으로 제한
+    overflow-hidden
 `;
 
 export const DropdownMenuItem = tw.div`
+    flex 
+    items-center 
+    justify-center 
     text-gray-700 
     hover:bg-gray-100 
     cursor-pointer 
     p-2 
     rounded-md
+    text-center
 `;
